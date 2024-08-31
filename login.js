bb0530479b36f492d1159ac0f9de7c1fdefacd5d
JesusAlfredoDGUEZ9 <jesusalfredo99dominguezporcayo@gmail.com>
2024-08-31 14:44:32 -0600

    CAMBIO

---
+++ login.js
@@ -0,0 +1,19 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById("username").value.toLowerCase();
+    const password = document.getElementById("password").value;
+    
+    const validCredentials = [
+        { username: "vane", password: "20" },
+        
+    ];
+    
+    const isValid = validCredentials.some(cred => cred.username === username && cred.password === password);
+    
+    if (isValid) {
+        window.location.href = "Bienvenida.html";
+    } else {
+        alert("Credenciales incorrectas. Inténtalo de nuevo.");
+    }
+});
